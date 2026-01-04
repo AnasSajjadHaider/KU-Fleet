69f59978f185a52f0422fa9d760f9450aa2403fe
Mustafa <mustafamubashir87@gmail.com>
2026-01-04 23:47:54 +0500

    Added auth

--- KU-Fleet-Backend/src/routes/authRoutes.ts
+++ KU-Fleet-Backend/src/routes/authRoutes.ts
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.post("/students/register",protect,adminOnly, registerStudent);
+router.post("/students/register", registerStudent);
 router.post("/students/login", loginStudent);
 
 
